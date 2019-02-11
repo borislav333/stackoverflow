a8cab337a39669df8836abc97a9b41e5a48d365b
Borislav <borislavqkimov@yahoo.com>
2019-02-11 23:49:16 +0200

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('questions','QuestionsController');
