aba9eb166b114fc0b0d260f7270f1053f54c95e2
Borislav <borislavqkimov@yahoo.com>
2019-02-11 23:24:00 +0200

    Question factory and seed

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Question::class, function (Faker $faker) {
+    $title=rtrim($faker->sentence(rand(5,10)),'.');
+    return [
+        'title'=>$title,
+        'slug'=>str_slug($title),
+        'body'=>$faker->paragraphs(rand(3,7),true),
+        'views'=>rand(0,10),
+        'answers'=>rand(0,10),
+        'votes'=>rand(-3,10),
+    ];
+});
